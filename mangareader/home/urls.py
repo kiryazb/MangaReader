d60a2f3db6fec95f7576855839df26c648ade187
kiryazb <zburivskii@mail.ru>
2023-11-01 00:41:46 +0300

    create home page html

--- mangareader/home/urls.py
+++ mangareader/home/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from django.views.generic import RedirectView
-
 from django.conf import settings
 from django.conf.urls.static import static
 
+from . import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='home/', permanent=False)),
-    path('home/', include('home.urls')),
+    path('', views.home_page, name='home_page')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
